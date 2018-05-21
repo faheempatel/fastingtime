2fd43f39aaaaf22f20208f58262c4c2513ac23a1
Faheem Patel <faheem@faheempatel.com>
2018-05-21 16:49:39 +0100

    Change desktop background colour

--- components/app.js
+++ components/app.js
@@ -20,6 +20,7 @@ import Footer from './Footer';
 const Container = styled.div`
   min-width: 375px;
   padding: 16px 24px;
+  background-color: #fff;
   @media only screen and (min-width: 600px) and (min-height: 720px) {
     box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1),
       0 5px 15px rgba(0, 0, 0, 0.07);
