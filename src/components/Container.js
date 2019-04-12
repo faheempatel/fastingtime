5fb6fa688204834d34094b2e3f588841d940999a
Faheem Patel <faheem@faheempatel.com>
2019-04-12 18:23:10 +0100

    Minor style fix

--- src/components/Container.js
+++ src/components/Container.js
@@ -13,16 +13,21 @@ const OuterContainer = styled('div')`
 
 const AppContainer = styled('div')`
   width: 100%;
+  min-height: 100vh;
   margin: auto;
   padding: 16px 24px;
   background-color: #fff;
+  overflow-y: scroll;
 
   @media only screen and (min-width: 600px) {
-    max-width: 375px;
+    width: 375px;
+    min-height: 100%;
   }
 
+  /* Tablet and above */
   @media only screen and (min-width: 600px) and (min-height: 720px) {
-    max-width: 400px;
+    width: 400px;
+    max-height: 674px;
     box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1),
       0 5px 15px rgba(0, 0, 0, 0.07);
     border-radius: 16px;
