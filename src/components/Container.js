e96a5f9e3818f97cf50a1a8228f540a10769f850
Faheem Patel <faheem@faheempatel.com>
2019-04-30 17:03:51 +0100

    Set desktop height explictly

--- src/components/Container.js
+++ src/components/Container.js
@@ -27,7 +27,7 @@ const AppContainer = styled('div')`
   /* Tablet and above */
   @media only screen and (min-width: 600px) and (min-height: 675px) {
     width: 400px;
-    max-height: 674px;
+    height: 674px;
     box-shadow: 0 15px 35px rgba(50, 50, 93, 0.1),
       0 5px 15px rgba(0, 0, 0, 0.07);
     border-radius: 16px;
