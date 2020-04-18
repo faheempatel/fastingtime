a3111e19fd14dfa6d351ce678ef255f7e248e0aa
Faheem Patel <faheem@faheempatel.com>
2020-04-18 18:03:36 +0100

    Revert "Remove css rule"
    This reverts commit b1597fc0763cb34134bccbc09932c708402fa0d1.

--- src/components/Container.js
+++ src/components/Container.js
@@ -22,6 +22,7 @@ const AppContainer = styled('div')`
   margin: auto;
   padding: 16px 24px;
   background-color: #fff;
+  overflow-y: scroll;
 
   @media only screen and (min-width: 600px) {
     width: 375px;
