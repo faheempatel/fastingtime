b3136954e5cbaaa26591d1f61ac0bba3498250de
Faheem Patel <faheem@faheempatel.com>
2020-04-25 17:01:24 +0100

    Revert "Remove offline support"
    This reverts commit 68c1909a229abeefe2f164487f12c543cef3332e.

--- preact.config.js
+++
@@ -1,5 +0,0 @@
-const RemoveServiceWorkerPlugin = require('webpack-remove-serviceworker-plugin');
-
-module.exports = function(config, env, helpers) {
-  config.plugins.push(new RemoveServiceWorkerPlugin());
-};
