7a1be1fb4553a2f976e37153cfcd1efa04049424
Faheem Patel <faheem@faheempatel.com>
2020-05-01 10:20:55 +0530

    Enable location menu

--- src/routes/App.js
+++ src/routes/App.js
@@ -40,7 +40,7 @@ const LOCATION_LS_KEY = 'selectedLocation';
 const DEFAULT_LOCATION = 'london';
 
 const FEATURE_FLAGS = {
-  LOCATION_MENU: false
+  LOCATION_MENU: true
 };
 
 export default class App extends Component {
